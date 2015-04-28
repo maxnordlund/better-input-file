eb0a726e985f8f1288b93b5491b849a1de49b4c6
Max Nordlund <max.nordlund@gmail.com>
2015-04-28 11:18:36 +0200

    Add Angular directive

---
+++ directive.js
@@ -0,0 +1,53 @@
+angular.module("angular-input-file", [])
+  .directive("input", function() {
+    return {
+      restrict: "E",
+      require: "?ngModel",
+      scope: {},
+      link: function link(scope, element, attr, ngModel) {
+        var handler
+
+        // This directive should only activate if the element it's attached to is
+        // an input[type=file] and it has a ngModel.
+        if (!ngModel || attr.type !== "file") return
+
+        // Enable automatic file loading
+        FileReader.auto(element[0], "DataURL")
+
+        // Make sure we set the pristine flag at link time
+        ngModel.$setPristine()
+
+        if (attr.multiple) {
+          ngModel.$isEmpty = function(value) {
+            return !value || value.length === 0
+          }
+
+          handler = function handleMultiple(event) {
+            ngModel.$setViewValue(Array.prototype.concat.apply([], event.files))
+          }
+        } else {
+          handler = function handleSingle(event) {
+            ngModel.$setViewValue(event.files[0])
+          }
+        }
+
+        // Attach event listners to FileReader events
+        element.on("loadallstart", handler)
+        element.on("loadall", function loadall(event) {
+          // Since event.file(s) is the same object/array as in "loadallstart"
+          // we need to make sure to update Angular
+          scope.$apply()
+        })
+
+        // Debug
+        var events = ["load", "loadallstart", "loadall", "progress", "error", "abort"]
+
+        events.forEach(function(name){
+          console.log("Listening on", name)
+          element.on(name, function logger() {
+            console.log(name, event)
+          })
+        })
+      }
+    }
+  })
